6c3e8bcdb3d7defc6448c60c3d91deb22149a892
kauangouveia <kauangouveia64@gmail.com>
2021-10-04 10:28:26 -0300

    chore : rota

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import app from "./app.js";
-app.listen(3333, () => {
-  console.log(`Servidor rodando na porta ${3333}`);
+app.listen(3334, () => {
+  console.log(`Servidor rodando na porta ${3334}`);
 });
